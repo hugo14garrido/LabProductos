0ebbee099fbf59eb88640ec72151dbb6614b37f4
= <=>
2018-06-02 14:35:28 -0600

    inicializando el proyecto

---
+++ src/main/java/com/labproductos/core/LabProductosApplication.java
@@ -0,0 +1,12 @@
+package com.labproductos.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabProductosApplication.class, args);
+	}
+}
